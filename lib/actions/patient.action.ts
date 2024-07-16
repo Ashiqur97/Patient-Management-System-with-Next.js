69eb2a026dde569a0b14a3470af127be85262b45
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-07-16 21:05:05 +0600

    code change

--- lib/actions/patient.action.ts
+++ lib/actions/patient.action.ts
@@ -1,17 +1,31 @@
-import { ID,Query } from "node-appwrite"
+import { ID,Models,Query } from "node-appwrite"
 import {users} from "../appwrite.config"
 
 
 export const createUser = async (user: CreateUserParams) => {
     try {
-        const newUser = await users.create(ID.unique(),user.email, user.phone,undefined);
+        const newUser = await users.create(
+            ID.unique(),
+            user.email, 
+            user.phone,
+            undefined,
+            user.name
+        );
+        console.log({newUser})
+
+        return parseStringify(newUser);
+
     } catch (error: any) {
         if(error && error?.code === 409) {
             const documents = await users.list([
-                Query.equal('email', [user.email])
+                Query.equal('email', [user.email]),
             ])
             return documents?.users[0];
         }
     }
+}
+
+function parseStringify(newUser: Models.User<Models.Preferences>) {
+    throw new Error("Function not implemented.");
 }
 
