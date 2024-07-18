5f35908c8889701f1034a81400ece107e4cc0c34
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-07-18 16:15:25 +0600

    code change

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -48,22 +48,47 @@ const RegisterForm =({user}:{user:User}) => {
   }
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+    <form onSubmit={form.handleSubmit(onSubmit)} 
+    className="space-y-12 flex-1">
       <section className="space-y-4">
         <h1 className="header">Welcome 👋</h1>
         <p className="text-dark-700">Let us know more about yourself.</p>
+      </section>
+
+      <section className="space-y-6">
+        <div className="mb-9 space-y-1">
+          <h2 className="sub-header">Personal Information</h2>
+        </div>
       </section>
 
       <CustomFormField
         fieldType={FormFieldType.INPUT}
         control={form.control}
         name="name"
-        label="Full name"
         placeholder="John Doe"
         iconSrc="/assets/icons/user.svg"
         iconAlt="user"
       />
 
+        <div className="flex flex-col gap-6 xl:flex-row">
+            
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567" iconSrc={""}        />
+        </div>
 
       <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
     </form>
