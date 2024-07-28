1f130a12b7dda53b1ccbfe630dd324a290af2272
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-07-28 15:36:35 +0600

    code change

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import PatientForm from "@/components/forms/PatientForm";
+import RegisterForm from "@/components/forms/RegisterForm";
 import { Button } from "@/components/ui/button";
+import { getUser } from "@/lib/actions/patient.action";
 import Image from "next/image";
 import Link from "next/link";
 
 
 export default function Home() {
+  const Register = async ({params:{userId}}: SearchParamProps) => {
+    const user = await getUser(userId);
+  }
+
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-          <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+          <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
               <Image 
                 src="/assets/icons/logo-full.svg"
                 height={1000}
@@ -16,16 +22,13 @@ export default function Home() {
                 alt="patient"
                 className="mb-12 h-10 w-fit"
               />
-          </div>
-          <PatientForm />
+          
+          <RegisterForm user={user} />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-              <p className="justify-items-end text-dark-600 xl:text-left">
-            ©2024 CarePlus
+      
+            <p className="copyright py-12">
+            © 2024 CarePlus
             </p> 
-            <Link href="/?admin=true" className="text-green-500">
-                Admin 
-            </Link>
           </div>
       </section>
       <Image 
--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -5,7 +5,6 @@ import ReactDatePicker from "react-datepicker";
 import { Control } from "react-hook-form";
 import PhoneInput from "react-phone-number-input";
 
-// import { Checkbox } from "./ui/checkbox";
 import {
   FormControl,
   FormField,
@@ -16,6 +15,7 @@ import {
 import { Input } from "./ui/input";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -144,6 +144,20 @@ const RenderInput = ({ field, props }: { field: any; props: CustomProps }) => {
       );
     case FormFieldType.SKELETON:
       return props.renderSkeleton ? props.renderSkeleton(field) : null;
+    case FormFieldType.CHECKBOX:
+      return (<FormControl>
+        <div className="flex items-center gap-4">
+            <Checkbox 
+              id={props.name}
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+            <label htmlFor={props.name} className="checkbox-label">
+                {props.label}
+            </label>
+        </div>
+      </FormControl>
+      );
     default:
       return null;
   }
