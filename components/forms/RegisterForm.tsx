3b6d398c60c0725db72832150b91bb9f79eec44a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-10 15:47:02 +0600

    code commit

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -214,6 +214,24 @@ const RegisterForm= ({user}: {user:User}) => {
               />
         </div>
 
+        <div className="flex flex-col gap-6 xl:flex-row">
+                <CustomFormField
+                  fieldType={FormFieldType.TEXTAREA}
+                  control={form.control}
+                  name="allergies"
+                  label="Allergies (if any)"
+                  placeholder="Peanuts, Penicillin, Pollen"
+                />
+
+              <CustomFormField
+              fieldType={FormFieldType.TEXTAREA}
+              control={form.control}
+              name="currentMedication"
+              label="Current medications"
+              placeholder="Ibuprofen 200mg, Levothyroxine 50mcg"
+              />
+        </div>
+
       <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
     </form>
   </Form>
