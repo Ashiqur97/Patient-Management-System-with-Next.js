bad2e4eae4ac3ff12e2829381dc871b4f06ec759
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-12 17:00:52 +0600

    code commit

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -19,6 +19,7 @@ import { E164Number } from "libphonenumber-js/core";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
+import { Checkbox } from "./ui/checkbox";
 
 
 interface CustomProps {
@@ -131,6 +132,22 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
         </FormControl>
 
       )
+
+      case FormFieldType.CHECKBOX:
+        return (
+          <FormControl>
+            <div className="flex items-center gap-4">
+              <Checkbox
+                id={props.name}
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+              <label htmlFor={props.name} className="checkbox-label">
+                {props.label}
+              </label>
+            </div>
+          </FormControl>
+        );
 
         case FormFieldType.SKELETON:
           return props.renderSkeleton ? props.renderSkeleton(field) : null;
