a2e5a95f9d03a77f1d303bf5f67a6e90755e565c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-16 15:36:08 +0600

    appointment system

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -27,6 +27,8 @@ import CustomFormField, { FormFieldType } from "../CustomFormField";
 import { FileUploader } from "../FileUploader";
 import SubmitButton from "../SubmitButton";
 
+
+
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -40,6 +42,7 @@ const RegisterForm = ({ user }: { user: User }) => {
       phone: user.phone,
     },
   });
+
 
   const onSubmit = async (values: z.infer<typeof PatientFormValidation>) => {
     setIsLoading(true);
