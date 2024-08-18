39757d3951576a64b87832a74d582e1c5f458248
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-18 17:45:45 +0600

    code commit

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -2,17 +2,15 @@ import { ID } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-export const  createAppointment = async () {
+export const  createAppointment = async (appointment: CreateAppointmentParams) {
     try {
-        const newPatient = await databases.createDocument(
+        const newAppointment = await databases.createDocument(
             DATABASE_ID!,
             APPOINTMENT_COLLECTION_ID!,
             ID.unique(),
-            {
-                ...patient
-            }
+            appointment
         )
-        return parseStringify(newPatient);
+        return parseStringify(newAppointment);
     } catch (error) {
         console.error(error);
     }
