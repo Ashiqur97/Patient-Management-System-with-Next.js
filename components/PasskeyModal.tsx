6c11bb4df59a678a1ba1f722bf00cd358862b0b2
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-23 16:31:31 +0600

    code change

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import {
     AlertDialog,
     AlertDialogAction,
@@ -12,10 +14,26 @@ import {
   } from "@/components/ui/alert-dialog"
 
 const PasskeyModal = () => {
+    const [open,setOpen] = useState(true);
+
   return (
-    <div>
-     PasskeyModal
-    </div>
+<AlertDialog open={open} onOpenChange={setOpen}>
+  <AlertDialogContent className='shad-alert-dialog'>
+    <AlertDialogHeader>
+      <AlertDialogTitle className='flex items-start justify-between'>
+        Admin Access Verification
+      </AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
   )
 }
 
