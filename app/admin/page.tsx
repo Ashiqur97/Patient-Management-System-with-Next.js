4e4c1c37d97b10dea91a7692a682cbeafb187e6e
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-08-24 15:52:51 +0600

    code updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Admin = () => {
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
-      <header>
-        
+      <header className='admin-header'>
+            <Link href="/" className='cursor-pointer'>
+                <Image 
+                    src="/assets/icons/logo-full.svg"
+                    height={32}
+                    width={162}
+                    alt='logo'
+                    className='h-8 w-fit'
+                />
+            </Link>
+
+            <p className='text-16-semibold'>Admin Dashboard</p>
       </header>
     </div>
   )
