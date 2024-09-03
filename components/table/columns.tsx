027708426d1628ed9154cf1ff240fd28e7e4ee4f
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-09-03 17:49:20 +0600

    code commit

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -16,10 +16,12 @@ import { formatDateTime } from "@/lib/utils"
 import Image from "next/image"
 import { Doctors } from "@/constants"
 import { AppointmentModal } from "../AppointmentModal."
+import { Appointment } from "@/types/appwrite.types"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
+  [x: string]: string
   patient: any
   schedule(schedule: any): unknown
   primaryPhysician: any
@@ -29,38 +31,45 @@ export type Payment = {
   email: string
 }
 
-export const columns: ColumnDef<Payment>[] = [
-
+export const columns: ColumnDef<Appointment>[] = [
   {
-    header: "ID",
-    cell: ({row}) => <p className="text-14-medium">{row.index + 1}</p>
+    header: "#",
+    cell: ({ row }) => {
+      return <p className="text-14-medium ">{row.index + 1}</p>;
+    },
   },
   {
     accessorKey: "patient",
     header: "Patient",
-    cell: ({ row }) => <p className="text-14-medium">{row.original.patient.name}</p>
-  },
-  {
-    accessorKey: 'status',
-    header: 'Status',
     cell: ({ row }) => {
-      <div className="min-w-[115px]">
-          <StatusBadge status={row.original.status} />
-      </div>
-    }
+      const appointment = row.original;
+      return <p className="text-14-medium ">{appointment.patient.name}</p>;
+    },
   },
   {
-    accessorKey: "schdule",
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <div className="min-w-[115px]">
+          <StatusBadge status={appointment.status} />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "schedule",
     header: "Appointment",
-    cell:({row}) => {
+    cell: ({ row }) => {
+      const appointment = row.original;
       return (
         <p className="text-14-regular min-w-[100px]">
-            {formatDateTime(row.original.schedule).dateTime}
+          {formatDateTime(appointment.schedule).dateTime}
         </p>
       );
-    }
+    },
   },
- 
   {
     accessorKey: "primaryPhysician",
     header: "Doctor",
@@ -68,7 +77,7 @@ export const columns: ColumnDef<Payment>[] = [
       const appointment = row.original;
 
       const doctor = Doctors.find(
-        (doctor: { name: any }) => doctor.name === appointment.primaryPhysician
+        (doctor) => doctor.name === appointment.primaryPhysician
       );
 
       return (
@@ -85,25 +94,32 @@ export const columns: ColumnDef<Payment>[] = [
       );
     },
   },
-
   {
     id: "actions",
     header: () => <div className="pl-4">Actions</div>,
     cell: ({ row }) => {
+      const appointment = row.original;
+
       return (
         <div className="flex gap-1">
-        <AppointmentModal 
-          type="schdule"
-          patientId={data.patient.$id}
-          userId={data.userId}
-          appointmentId={data}
-          title="Schedule Appointment"
-          description="Schedule an appointment for the patient"
-
-        />
-            <AppointmentModal patientId={""} userId={""} type={"schedule"} title={""} description={""} />
+          <AppointmentModal
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="schedule"
+            title="Schedule Appointment"
+            description="Please confirm the following details to schedule."
+          />
+          <AppointmentModal
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="cancel"
+            title="Cancel Appointment"
+            description="Are you sure you want to cancel your appointment?"
+          />
         </div>
-      )
+      );
     },
   },
-]
+];
